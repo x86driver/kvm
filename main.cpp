a3cf0e74e20a7876c9df01d190807cbe43b5ff1c
Shane Lin <253692+x86driver@users.noreply.github.com>
2024-07-20 08:13:12 +0800

    Add KVM_CREATE_VM and more debug message

--- main.cpp
+++ main.cpp
@@ -5,32 +5,43 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <linux/kvm.h>
+#include <stdint.h>
 
 int main() {
     struct kvm_sregs sregs;
     int ret;
-    int kvmfd = open("/dev/kvm", O_RDWR);
+    int kvmfd = open("/dev/kvm", O_RDWR | O_CLOEXEC);
     if (kvmfd == -1) {
         perror("open /dev/kvm");
+        return 1;
+    }
+    int vmfd = ioctl(kvmfd, KVM_CREATE_VM, 0);
+    if (vmfd == -1) {
+        perror("KVM_CREATE_VM");
         return 1;
     }
     int version = ioctl(kvmfd, KVM_GET_API_VERSION, NULL);
     printf("KVM API version: %d\n", version);
     unsigned char *ram = (unsigned char*)mmap(NULL, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    if (ram == MAP_FAILED) {
+        perror("mmap");
+        return 1;
+    }
     int kfd = open("test.bin", O_RDONLY);
     read(kfd, ram, 4096);
+    close(kfd);
     struct kvm_userspace_memory_region region = {
         .slot = 0,
-        .guest_phys_addr = 0,
+        .guest_phys_addr = 0x0,
         .memory_size = 0x1000,
-        .userspace_addr = (unsigned long)ram,
+        .userspace_addr = (uint64_t)ram,
     };
-    ret = ioctl(kvmfd, KVM_SET_USER_MEMORY_REGION, &region);
+    ret = ioctl(vmfd, KVM_SET_USER_MEMORY_REGION, &region);
     if (ret == -1) {
         perror("KVM_SET_USER_MEMORY_REGION");
         return 1;
     }
-    int vcpufd = ioctl(kvmfd, KVM_CREATE_VCPU, 0);
+    int vcpufd = ioctl(vmfd, KVM_CREATE_VCPU, 0);
     if (vcpufd == -1) {
         perror("KVM_CREATE_VCPU");
         return 1;
@@ -45,10 +56,19 @@ int main() {
     sregs.cs.base = 0;
     sregs.cs.selector = 0;
     ret = ioctl(vcpufd, KVM_SET_SREGS, &sregs);
+    if (ret == -1) {
+        perror("KVM_SET_SREGS");
+        return 1;
+    }
     struct kvm_regs regs = {
         .rip = 0,
+        .rflags = 0x2,
     };
     ret = ioctl(vcpufd, KVM_SET_REGS, &regs);
+    if (ret == -1) {
+        perror("KVM_SET_REGS");
+        return 1;
+    }
     while (1) {
         ret = ioctl(vcpufd, KVM_RUN, NULL);
         if (ret == -1) {
